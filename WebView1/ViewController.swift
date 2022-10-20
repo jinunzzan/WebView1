434e888039de59f02504e8b4015de690e7a80330
jinunzzan <lovebox6607@gmail.com>
2022-10-21 03:10:10 +0900

    WebView 사용해보기, 버튼 만들어서 goBack, reload, goForward 해보기!

--- WebView1/ViewController.swift
+++ WebView1/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var destinationURL: String = "http://www.daum.net"
+    
+    @IBOutlet weak var webSearchBar: UISearchBar!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        openWebPage(to: destinationURL)
+        webSearchBar.delegate = self
     }
+    
+    func openWebPage(to urlStr: String) {
+        guard let url = URL(string: urlStr) else {
+            print("Invalid URL")
+            return
+        }
+        let request = URLRequest(url:url)
+        webView.load(request)
+    }
+    
+    @IBAction func goBackButtonClicked(_ sender: Any) {
+        if webView.canGoBack {
+            webView.goBack()
+        }
+    }
+    
+    @IBAction func reloadButtonClicked(_ sender: Any) {
+        webView.reload()
+    }
+    
+    @IBAction func goForwardButtonClicked(_ sender: Any) {
+        if webView.canGoForward {
+            webView.goForward()
+        }
+    }
+    
 
 
 }
 
+extension ViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        openWebPage(to: searchBar.text!)
+    }
+}
